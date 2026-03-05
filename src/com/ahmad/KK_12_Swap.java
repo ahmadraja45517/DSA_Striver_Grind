5b2dc2ca1765b5c2cc6f9ebbd5cce4f215b8c5b9
Ahmad Raja <ahmadraza45517@gmail.com>
2026-03-05 23:26:59 +0530

    feat: implement integer swapping logic using temporary variable

--- src/com/ahmad/KK_12_Swap.java
+++ src/com/ahmad/KK_12_Swap.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class KK_12_Swap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        System.out.println("Enter the number");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
