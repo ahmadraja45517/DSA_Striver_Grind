a16f4f98d30b2b1917adabf973e41f8d9dab74e7
Ahmad Raja <ahmadraza45517@gmail.com>
2026-03-06 00:04:54 +0530

    feat: implement variable arguments (VarArgs) for multiple data types

---
+++ src/com/ahmad/KK_14_VarArgs.java
@@ -0,0 +1,19 @@
+package com.ahmad;
+
+import java.util.Arrays;
+
+public class KK_14_VarArgs {
+    public static void main(String[] args) {
+        fun(1, 2, 3, 4, 5);
+
+        multiple(2, 3, "Ahmad", "Amit", "Harshit");
+    }
+
+    static void multiple(int a, int b, String ...v) {
+        System.out.println("a: " + a + ", b: " + b + ", v: " + Arrays.toString(v));
+    }
+
+    static void fun(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+}
