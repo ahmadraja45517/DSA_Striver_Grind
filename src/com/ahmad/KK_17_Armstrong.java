425e77088ed164fb704e5fd2e63d7d2c2282abea
Ahmad Raja <ahmadraza45517@gmail.com>
2026-03-08 22:31:40 +0530

    feat: implement logic to verify 3-digit Armstrong numbers

---
+++ src/com/ahmad/KK_17_Armstrong.java
@@ -0,0 +1,28 @@
+package com.ahmad;
+
+import java.util.Scanner;
+
+public class KK_17_Armstrong {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = in.nextInt();
+        System.out.println(armstrong(n));
+    }
+
+    static boolean armstrong(int a){
+        int sum = 0;
+        int original = a;
+        while(a > 0){
+            int rem = a % 10;
+            int cube = rem * rem * rem;
+            a = a / 10;
+            sum = sum + cube;
+        }
+        if (sum == original){
+            return true;
+        }
+        return false;
+    }
+}
