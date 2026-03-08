f23167dc85b9ed07d56432667531bc2314e87702
Ahmad Raja <ahmadraza45517@gmail.com>
2026-03-08 21:49:30 +0530

    feat: implement method overloading for integer and string types

---
+++ src/com/ahmad/KK_15_Overloading.java
@@ -0,0 +1,23 @@
+package com.ahmad;
+
+import java.util.Arrays;
+
+public class KK_15_Overloading {
+    public static void main(String[] args) {
+        fun(67);
+        fun("Ahmad Raja");
+        demo("Ahmad", "Amit", "Ankit");
+    }
+
+    static void demo(String ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void fun(int a){
+        System.out.println(a);
+    }
+
+    static void fun(String name){
+        System.out.println(name);
+    }
+}
