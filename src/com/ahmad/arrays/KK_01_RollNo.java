5d32a76b7a9d3d5d55a3f982bec68e1716267c73
Ahmad Raja <ahmadraza45517@gmail.com>
2026-03-09 22:42:39 +0530

    Implemented Array declaration, initialization, and memory allocation syntax

---
+++ src/com/ahmad/arrays/KK_01_RollNo.java
@@ -0,0 +1,29 @@
+package com.ahmad.arrays;
+
+public class KK_01_RollNo {
+    public static void main(String[] args) {
+
+        //storing 5 roll no
+        //int[] rnos = new int[5];
+        //or directly
+        //int[] rnos2 = {23, 15, 45, 12, 19};
+
+        int[] ros; //declaration
+        ros = new int[10];//initialization
+        ros[0] = 1;
+        ros[1] = 2;
+        ros[2] = 3;
+        ros[3] = 4;
+        ros[4] = 5;
+        ros[5] = 6;
+        ros[6] = 7;
+        ros[7] = 8;
+        ros[8] = 9;
+        ros[9] = 10;
+
+        System.out.println(ros[5]);
+
+        String[] arr = new String[10];
+        System.out.println(arr[5]);
+    }
+}
