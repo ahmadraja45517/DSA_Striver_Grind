1468446b5d0d92898affcfa8d4b3458fc5be98b2
Ahmad Raja <ahmadraza45517@gmail.com>
2026-03-12 00:28:31 +0530

    feat: implement in-place array element swapping using a temporary variable

---
+++ src/com/ahmad/arrays/Swap.java
@@ -0,0 +1,18 @@
+package com.ahmad.arrays;
+
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 16, 25, 36};
+        swap(arr, 1, 3);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+
+    }
+}
